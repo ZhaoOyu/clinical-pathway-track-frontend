8af48e387bda2d5eac03d2333c2d0b7cb76a16d6
ZhaoOyu <113964370+ZhaoOyu@users.noreply.github.com>
2023-07-19 16:05:50 +0800

    feat:add table in dashboard

--- src/components/MonitorPlatfrom/component/Dashboard.vue
+++ src/components/MonitorPlatfrom/component/Dashboard.vue
@@ -1,10 +1,34 @@
 <template>
-  <div class="charts-container">
-    <div class="box" v-for="(item, index) in itemList" :key="index">
-      <div :class="item.iconName" class="icon" size="50px"></div>
-      <div class="title">{{ item.title }}</div>
-      <div class="Etitle">{{ item.Etitle }}</div>
-      <div class="score">{{ item.score }}</div>
+  <div>
+    <div class="charts-container">
+      <div class="box" v-for="(item, index) in itemList" :key="index">
+        <div :class="item.iconName" class="icon" size="50px"></div>
+        <div class="title">{{ item.title }}</div>
+        <div class="Etitle">{{ item.Etitle }}</div>
+        <div class="score">{{ item.score }}</div>
+      </div>
+    </div>
+    <div>
+      <table class="realtime-table">
+        <thead>
+          <tr>
+            <th>指标名称</th>
+            <th>当前值</th>
+            <th>参考范围</th>
+            <th>状态</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in realtimeData" :key="item.id">
+            <td>{{ item.indicator }}</td>
+            <td>{{ item.value }}</td>
+            <td>{{ item.reference }}</td>
+            <td :class="getStatusClass(item.value, item.reference)">
+              {{ getStatusText(item.value, item.reference) }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -14,6 +38,12 @@ export default {
   name: "Dashboard",
   data() {
     return {
+      realtimeData: [
+        { id: 1, indicator: "治疗成果", value: 80, reference: "70 - 90" },
+        { id: 2, indicator: "医疗技术", value: 85, reference: "80 - 95" },
+        { id: 3, indicator: "医护人员素质", value: 78, reference: "75 - 85" },
+        // 添加更多的指标数据
+      ],
       itemList: [
         {
           title: "医疗质量",
@@ -34,7 +64,54 @@ export default {
           iconName: "el-icon-edit-outline",
         },
       ],
+      chartOptions_1: {
+        tooltip: {},
+        radar: {
+          indicator: [
+            { name: "治疗成果", max: 10 },
+            { name: "医疗技术", max: 10 },
+            { name: "医护人员素质", max: 10 },
+            { name: "诊断准确性", max: 10 },
+            { name: "医疗安全", max: 10 },
+            { name: "住院环境", max: 10 },
+            { name: "医患沟通", max: 10 },
+            { name: "医疗费用", max: 10 },
+          ],
+        },
+        series: [
+          {
+            name: "医院A",
+            type: "radar",
+            data: [
+              {
+                value: [8.5, 9.0, 8.0, 7.5, 8.5, 7.5, 8.0, 7.0],
+                name: "医院A",
+              },
+            ],
+          },
+        ],
+      },
     };
+  },
+  methods: {
+    getStatusClass(value, reference) {
+      const [min, max] = reference.split(" - ");
+      value = parseFloat(value);
+      if (value >= parseFloat(min) && value <= parseFloat(max)) {
+        return "normal";
+      } else {
+        return "abnormal";
+      }
+    },
+    getStatusText(value, reference) {
+      const [min, max] = reference.split(" - ");
+      value = parseFloat(value);
+      if (value >= parseFloat(min) && value <= parseFloat(max)) {
+        return "正常";
+      } else {
+        return "异常";
+      }
+    },
   },
 };
 </script>
@@ -71,5 +148,31 @@ export default {
     font-size: 50px;
     color: aqua;
   }
+}
+.realtime-table {
+  color: aqua;
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+.realtime-table th,
+.realtime-table td {
+  padding: 8px;
+  border: 1px solid #ddd;
+}
+
+.realtime-table th {
+  background-color: #f5f5f5;
+  text-align: left;
+}
+
+.realtime-table td.normal {
+  color: green;
+}
+
+.realtime-table td.abnormal {
+  color: red;
+  font-weight: bold;
 }
 </style>
--- src/components/MonitorPlatfrom/component/Quality.vue
+++ src/components/MonitorPlatfrom/component/Quality.vue
@@ -1,91 +1,48 @@
 <template>
-  <div class="charts-container"></div>
+  <div class="charts-container">
+    <chart-template
+      :options="chartOptions_1"
+      title="医疗质量雷达图"
+      width="50%"
+    />
+  </div>
 </template>
 
 <script>
+import ChartTemplate from "./ChartTemplate.vue";
+
 export default {
   name: "Quality",
+  components: {
+    ChartTemplate,
+  },
   data() {
     return {
-      options_1: {
-        // 饼图的配置选项
-        title: {
-          text: "医疗服务收入占比", // 标题文本
-          textStyle: {
-            fontSize: 20, // 标题字体大小
-            fontWeight: "bold", // 标题字体粗细
-            color: "#ffffff",
-          },
-          left: "center", // 标题水平居中对齐
-          top: 20, // 标题距离容器顶部的距离
+      chartOptions_1: {
+        label: {
+          show: true,
+          formatter: "{c}",
+        },
+        radar: {
+          indicator: [
+            { name: "治疗成果", max: 10 },
+            { name: "医疗技术", max: 10 },
+            { name: "医护人员素质", max: 10 },
+            { name: "诊断准确性", max: 10 },
+            { name: "医疗安全", max: 10 },
+            { name: "住院环境", max: 10 },
+            { name: "医患沟通", max: 10 },
+            { name: "医疗费用", max: 10 },
+          ],
         },
         series: [
           {
-            type: "pie",
-            radius: "50%",
-            title: "c",
-            center: ["50%", "50%"],
+            type: "radar",
             data: [
-              { name: "医疗服务收入", value: 30 },
-              { name: "其他收入", value: 70 },
-            ],
-            itemStyle: {
-              normal: {
-                color: function (params) {
-                  const colorList = ["#c23531", "#1E90FF"];
-                  return colorList[params.dataIndex];
-                },
+              {
+                value: [8.5, 9.0, 8.0, 7.5, 8.5, 7.5, 8.0, 7.0],
               },
-            },
-            animationDuration: 2000,
-            label: {
-              // 标签配置
-              position: "outer", // 标签位置设为外部
-              alignTo: "labelLine", // 标签对齐线
-              formatter: "{b}：{d}%", // 标签内容格式化，包含百分比和对应数据项值
-              fontSize: "15",
-            },
-          },
-        ],
-      },
-      options_2: {
-        // 饼图的配置选项
-        title: {
-          text: "按病种付费比例", // 标题文本
-          textStyle: {
-            fontSize: 20, // 标题字体大小
-            fontWeight: "bold", // 标题字体粗细
-            color: "#ffffff",
-          },
-          left: "center", // 标题水平居中对齐
-          top: 20, // 标题距离容器顶部的距离
-        },
-        series: [
-          {
-            type: "pie",
-            radius: "50%",
-            title: "c",
-            center: ["50%", "50%"],
-            data: [
-              { name: "按病种付费", value: 60 },
-              { name: "其他付费方式", value: 30 },
             ],
-            itemStyle: {
-              normal: {
-                color: function (params) {
-                  const colorList = ["#c23531", "#1E90FF"];
-                  return colorList[params.dataIndex];
-                },
-              },
-            },
-            animationDuration: 2000,
-            label: {
-              // 标签配置
-              position: "outer", // 标签位置设为外部
-              alignTo: "labelLine", // 标签对齐线
-              formatter: "{b}：{d}%", // 标签内容格式化，包含百分比和对应数据项值
-              fontSize: "15",
-            },
           },
         ],
       },
@@ -94,10 +51,4 @@ export default {
 };
 </script>
 <style lang='scss' scoped>
-.charts-container {
-  height: 100%;
-  margin: auto;
-  display: flex;
-  justify-content: space-around;
-}
 </style>
